966be80a7c5662debebf9226d0f6d5aa7a463235
AgentPee <perryian01@gmail.com>
2025-11-20 23:55:31 +0800

    another fix

--- QuickClinique/Program.cs
+++ QuickClinique/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
 using QuickClinique.Models;
 using QuickClinique.Services;
 using QuickClinique.Middleware;
